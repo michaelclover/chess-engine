b81c17663e2232bc5026968ed054aa4bfd3ca492
michaelclover <michaelac2030@gmail.com>
2022-02-07 00:23:40 +0000

    center board, handle square toggling, and use normalize.css to reset style.

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isClickToggled: false,
+            number: props.number,
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
     render() {
+        // unicode blank character to stop the button moving when toggled.
+        const content = this.state.isClickToggled ? this.state.number : "\u2060"; 
         return (
-            <button class="square"></button>
+            <button class="square" onClick={this.handleClick}>{content}</button>
         );
+    }
+
+    handleClick() {
+        this.setState(prevState => ({
+            isClickToggled: !prevState.isClickToggled
+        }));
     }
 }
 
